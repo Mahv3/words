3bbf2b789619c9c0a3f2cfcbbe968471c16ca0ca
masaya <spag9hc9@yahoo.co.jp>
2022-04-19 00:19:43 +0900

    envファイル追加

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: "AIzaSyCrHEEJkKrdJBegQMo7T94xcAI9PneCzb4",
-        authDomain: "words-app-c009c.firebaseapp.com",
-        projectId: "words-app-c009c",
-        storageBucket: "words-app-c009c.appspot.com",
-        messagingSenderId: "809697591167",
-        appId: "1:809697591167:web:dc8cedf84ddc87a39b644a"
+        apiKey: process.env.NUXT_API_KEY,
+        authDomain: process.env.NUXT_AUTH_DOMAIN,
+        projectId: process.env.NUXT_PROJECT_ID,
+        storageBucket: process.env.NUXT_STORAGE_BUCKET,
+        messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID,
+        appId: process.env.NUXT_APP_ID
     })
 }
 export default firebase
